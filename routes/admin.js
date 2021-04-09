64b2ec7840f54acd27536aa0f73960118b315654
manshad <manshadchangampalli010@gmail.com>
2021-04-09 12:08:37 +0530

    created add products and view products pages successfully

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,40 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { admin:true });
+  products=[
+    {
+    name:'allensolly',
+    category:'shirts',
+    price:'499',
+    image:'https://images-na.ssl-images-amazon.com/images/I/513favBlpgL._UX569_.jpg'
+
+  },
+  {
+    name:'levis',
+    category:'shirts',
+    price:'499',
+    image:'https://images-na.ssl-images-amazon.com/images/I/513favBlpgL._UX569_.jpg'
+
+  },
+  {
+    name:'us polo',
+    category:'shirts',
+    price:'499',
+    image:'https://images-na.ssl-images-amazon.com/images/I/513favBlpgL._UX569_.jpg'
+
+  },
+  {
+    name:'otto',
+    category:'shirts',
+    price:'499',
+    image:'https://images-na.ssl-images-amazon.com/images/I/513favBlpgL._UX569_.jpg'
+
+  },
+]
+  res.render('admin/view-products', { admin:true,products });
 });
+router.get('/addproduct',function(req,res){
+ res.render('admin/add-products')
+})
 
 module.exports = router;
