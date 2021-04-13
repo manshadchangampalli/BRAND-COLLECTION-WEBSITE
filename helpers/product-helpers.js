cf87ea011dde8a6ee3e9a733f5f52f2b6023109a
manshad <manshadchangampalli010@gmail.com>
2021-04-13 09:14:08 +0530

    add the feature of edit  product . at that time i get an error where i appy comma',' instead of '.' ...but i can find rappidly

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -23,5 +23,27 @@ deletProduct:(prodId)=>{
             resolve(response)
         })
     })
+},
+getProductDetails:(proId)=>{
+    return new Promise((resolve,reject)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+            
+            resolve(product)
+        })
+  })
+},
+updateProduct:(proId,proDetails)=>{
+    return new Promise((resolve,reject)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:objectId(proId)},{
+            $set:{
+                Name:proDetails.Name,
+                Category:proDetails.Category,
+                Price:proDetails.Price
+
+            }
+        }).then((response)=>{
+            resolve()
+        })
+    })
 }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -38,5 +38,15 @@ router.get('/delete-product/:id',(req,res)=>{
     res.redirect('/admin/')
   })
 })
+router.get('/edit-product/:id',async(req,res)=>{
+  let product=await productHelpers.getProductDetails(req.params.id)
+  
+  res.render('admin/edit-product',{product})
+})
+router.post('/edit-product/:id',(req,res)=>{
+  productHelpers.updateProduct(req.params.id,req.body).then(()=>{
+    res.redirect('/admin')
+  })
+})
 
 module.exports = router;
