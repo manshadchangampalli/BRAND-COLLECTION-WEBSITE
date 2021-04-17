6cd7d236b6e5491692209fcbf8d929d7a1b6537a
manshad <manshadchangampalli010@gmail.com>
2021-04-17 13:28:45 +0530

    add to cart products select from mongodb

--- routes/user.js
+++ routes/user.js
@@ -60,7 +60,9 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin,(req,res)=>{
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products);
   res.render('user/cart')
 })
 router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
